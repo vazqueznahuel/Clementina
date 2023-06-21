5deffe9f833148af4fa6aaaaebdc9f585a3a1ea1
Facundo Hermosilla <facundohermosillaepet20@gmail.com>
2023-06-20 23:42:44 -0300

    Agregando importancion del componente Form a App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Label from './components/label/label.jsx'
+import Form from './components/form/form.jsx'
 
 function App() {
   return (
     <view>
-      
+
     </view>
   );
 }
