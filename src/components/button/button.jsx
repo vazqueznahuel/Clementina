ee2a645bcd2a87ac72d8038cd28119b5a6d4ea52
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-07-05 13:15:14 -0300

    Agregando atributo name al componente Button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,7 @@
 function  Button (props){
     return(
     <div>
-        <button type={props.typeButton} value={props.valueButton}></button>
+        <button type={props.typeButton} name={props.nameButton} value={props.valueButton}></button>
     </div>
     );
 }
