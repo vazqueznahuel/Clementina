25612985f05891e5a88f694bd4a1ad4187b9bc8f
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-07-05 14:16:28 -0300

    Eliminando el props de value en el componente Input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,7 +1,7 @@
 function Input (props){
     return (
         <div>
-            <input type={props.typeInput} name={props.nameInput} value={props.valueInput} placeholder={props.placeHolderInput} required/>
+            <input type={props.typeInput} name={props.nameInput} placeholder={props.placeHolderInput} required/>
         </div>
     );
 }
