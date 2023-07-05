9612d6550e181295e7c70335ef8e7b0183cf98aa
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-07-05 13:58:30 -0300

    Agregando un props de name al componente Input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,7 +1,7 @@
 function Input (props){
     return (
         <div>
-            <input type={props.typeInput} />
+            <input type={props.typeInput} name={props.nameInput} />
         </div>
     );
 }
