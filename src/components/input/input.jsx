fbe0a7848c28a9b5c674a7075d02fc8b5503495f
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-07-05 13:59:54 -0300

    Agregando un props de place holder al componente Input

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,7 +1,7 @@
 function Input (props){
     return (
         <div>
-            <input type={props.typeInput} name={props.nameInput} value={props.valueInput} />
+            <input type={props.typeInput} name={props.nameInput} value={props.valueInput} placeholder={props.placeHolderInput}/>
         </div>
     );
 }
