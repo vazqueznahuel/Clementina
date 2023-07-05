fb3c6f48986a986ce41ded3edee2e1f22dc1859f
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-07-05 13:30:16 -0300

    Agregando contentButton en el componente form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -5,7 +5,7 @@ function Form(props) {
     <div>
         <form method="POST">
             <Label infoLabel="Ingrese su nombre"/>
-            <Button typeButton="submit" nameButton="button-submit" valueButton="submit-form"/>
+            <Button typeButton="submit" nameButton="button-submit" valueButton="submit-form" contentButton="Enviar"/>
         </form>
     </div>
     );
