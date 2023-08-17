b6bf6b1ce549e2a28ac236229a8d0ef79c16d690
facundohermosillaepet20@gmail.com <rodriguez.facundo.epet20@gmail.com>
2023-08-17 15:03:56 -0300

    Agregando importacion del css al componente button

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,7 +1,9 @@
+import '../../App.css'
+
 function  Button (props){
     return(
     <div>
-        <button type={props.typeButton} name={props.nameButton} value={props.valueButton}>{props.contentButton}</button>
+        <button className={props.className} type={props.typeButton} name={props.nameButton} value={props.valueButton}>{props.contentButton}</button>
     </div>
     );
 }
