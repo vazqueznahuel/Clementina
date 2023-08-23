d2247d7c5c0bacfa670842479a3300ad2d0cb52b
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-08-23 11:44:49 -0300

    Modificando la configuracion de firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.RECT_APP_FIREBASE_APIKEY,
-  authDomain:process.env.RECT_APP_FIREBASE_AUTHDOMAIN,
-  projectId:process.env.RECT_APP_FIREBASE_PROJECTID,
-  storageBucket:process.env.RECT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId:process.env.RECT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId:process.env.RECT_APP_FIREBASE_APPID,
+  apiKey: "AIzaSyBV09NxbErg6RetvXcFVeesYdi3LR2jp44",
+  authDomain: "e-pets-76548.firebaseapp.com",
+  projectId: "e-pets-76548",
+  storageBucket: "e-pets-76548.appspot.com",
+  messagingSenderId: "1029060292650",
+  appId: "1:1029060292650:web:40838bb182f983e8c71353"
 };
 
 // Initialize Firebase
