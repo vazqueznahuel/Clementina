57e7238155d140aa4ea1ff1dc4f1985114323fec
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-09-11 13:56:59 -0300

    Modificaciones en el componenter formulario

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -6,29 +6,54 @@ import Button from '../button/button.jsx'
 import Input from '../input/input.jsx'
 import '../../App.css'
 
+import appFirebase from '../../firebase/firebaseConfig.js'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+const auth = getAuth(appFirebase);
+
 function Form(props) {
-    const [ email, setEmail ] = useState('');
-    const [ password, setPassword ] = useState('');
+    
+  const [registrando, setRegistrando] = useState(false);
 
-    const submit = () => {
-      console.log(email, password);
-    }
-
+  const functAutenticacion = async (e) =>{  
+      e.preventDefault();
+      const correo = e.target.email.value;
+      const contraseña = e.target.password.value;
+      
+      if(registrando){
+        try {
+          await createUserWithEmailAndPassword(auth, correo, contraseña);   
+        } catch (error) {
+          alert("Asegurese que la contraseña tenga 8 caracteres")
+        }
+          
+      }
+      else{
+      
+        try {
+          await signInWithEmailAndPassword(auth, correo, contraseña);
+        } catch (error) {
+          alert ("El correo es incorrecto");
+        }
+      }
+  }
     return (
-        <div className="form-container">
-          <h2 className="form-title">Formulario de Registro</h2>
-          <Label className="info-label" infoLabel="Ingrese su correo electronico" />
-          <input className='input-field' type='email' id='email' onChange={(ev) => setEmail(ev.target.value)}/>
-          <Label className="info-label" infoLabel="Ingrese su Contraseña" />
-          <input className='input-field' type='password' id='password' onChange={(ev) => setPassword(ev.target.value)}/>
-          <button className="button" onClick={submit}>iniciar session</button>
-          <Button
-            className="button"
-            typeButton="reset"
-            nameButton="button-reset"
-            valueButton="reset-form"
-            contentButton="Restablecer"
-          />
+
+    <div className="form-container">
+        <h2 className='form-title'> {registrando ? "Iniciar Sesion" : "Registrate"} </h2>
+
+        <form onSubmit={functAutenticacion}>
+
+          <Label className="info-label" infoLabel="Ingrese email"/>
+          <input className='input-field' type='text' placeholder='Ingresar email' id='email'/>
+                       
+          <Label className="info-label" infoLabel="Ingrese contraseña"/>
+          <input className='input-field' type='password' placeholder='Ingresar contraseña ' id='password'/>
+           
+          <button className='button'>{registrando ? "Registrate" : "Inicia Sesion"}</button>
+          
+        </form>
+                  
+        <h4> {registrando ? "Si ya tienes cuenta" : "No tienes cuenta"} <button className='button' onClick={()=>setRegistrando(!registrando)}>{registrando ? "Iniciar Sesion" : "Registrate"}</button></h4>
       </div>
     );
     }
