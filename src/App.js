29ad60f195c755060cfc16bc262d258c21cf4e63
adrian vazquez <nahuel.vazquez@init-dev.com.ar>
2023-09-27 02:01:41 -0300

    arreglando problemas con array de props de modelos

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ModelViewer from './components/ModelViewer';
 
 function App() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [previousIndex, setPreviousIndex] = useState(null);
-
   const modelPaths = [
     '/widiModelo.gltf',
     '/epet8.gltf',
+    '/epet5.gltf'
     // Agrega más rutas de modelos 
   ];
 
   const modelPositions = [
     [0, -1, 0],
     [0, -1, 0],
+    [0, -1, 0]
     // Agrega más posiciones de modelos 
   ];
 
+  const [modelsLoaded, setModelsLoaded] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const loadModels = async () => {
+    const modelPromises = modelPaths.map(async (path) => {
+      const response = await fetch(path);
+      const modelData = await response.arrayBuffer();
+      // Aquí puedes cargar el modelo en tu visor 3D o realizar cualquier otra operación necesaria
+    });
+
+    await Promise.all(modelPromises);
+    setModelsLoaded(true);
+  };
+
+  useEffect(() => {
+    loadModels();
+  }, []);
+
   const handleNextModel = () => {
-    setPreviousIndex(currentIndex); 
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % modelPaths.length);
+    if (modelsLoaded) {
+      console.log("Siguiente Modelo:", modelPaths[currentIndex]); // Imprime la ruta del modelo
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % modelPaths.length);
+    } else {
+      console.log("Los modelos aún se están cargando. Por favor, espera...");
+    }
   };
 
   const handlePreviousModel = () => {
-    if (previousIndex !== null) {
-      setCurrentIndex(previousIndex); 
-      setPreviousIndex((prevIndex) => (prevIndex - 1 + modelPaths.length) % modelPaths.length);
+    if (modelsLoaded) {
+      console.log("Modelo Anterior:", modelPaths[currentIndex]); // Imprime la ruta del modelo
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + modelPaths.length) % modelPaths.length);
+    } else {
+      console.log("Los modelos aún se están cargando. Por favor, espera...");
     }
   };
 
