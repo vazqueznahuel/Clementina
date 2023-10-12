0dfe94088e4f0e9653d47b0dba314f69002c0040
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:23:27 -0300

    Añadiendo front al componente nav

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -6,6 +6,22 @@ function Nav(props) {
         className={`hamburger ${isMenuOpen ? "is-active" : ""}`}
         onClick={toggleMenu}
       >
+        <div className="_layer -top"></div>
+        <div className="_layer -mid"></div>
+        <div className="_layer -bottom"></div>
+   
+        <nav className={`menuppal ${isMenuOpen ? "is_active" : ""}`}>
+          <ul>
+            <li><button onClick={() => {
+                    signOut(auth);
+                }}>Cerrar Sesion</button></li>
+            <li><a href="generarNota">.</a></li>
+            <li><a href="comunicaciones">.</a></li>
+            <li><a href="personales">.</a></li>
+            <li><a href="retiro">.</a></li>
+          </ul>
+        </nav>
+
       </div>
     );
   }
