478cbd2f1e8dc399f4648264ebddcf8e74144118
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:33:41 -0300

    Añadiendo importaciones en App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React, { useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore'; // Importa collection y getDocs
-import db from './firebase/firebaseConfig.js';
 import './App.css';
-import Form from './components/form/form.jsx';
-import Home from './components/home/home.jsx';
-import appFirebase from './firebase/firebaseConfig.js';
-import { getAuth, onAuthStateChanged } from 'firebase/auth'
-const auth = getAuth(appFirebase);
-
+import AppRouter from './routes/routes.jsx';
 
 function App() {
 
-    const [ usuario, setUsuario ] = useState(null);
-
-    onAuthStateChanged(auth, (usuarioFirebase)=> {
-      if (usuarioFirebase){
-        setUsuario(usuarioFirebase)
-      }
-      else{
-        setUsuario(null)
-      }
-    })
-
-
   return (
     <div>
-      {usuario ? <Home correoUsuario = {usuario.email}/> : <Form/>}
+      
     </div>
   );
 }
