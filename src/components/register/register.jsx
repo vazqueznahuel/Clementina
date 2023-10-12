4fbeae21cda161d6e1b559131b3c65418f3f8dc2
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:26:11 -0300

    Añadiendo front al componente register

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -4,6 +4,26 @@ function Register(props) {
     return (
 
     <div className="form-container">
+      <h2 className='form-title'>Registrar</h2>
+
+      <form onSubmit={functAutenticacion}>
+
+        <Label className="info-label" infoLabel="Ingrese email"/>
+        <input className='input-field' type='text' placeholder='Ingresar email' id='email'/>
+                    
+        <Label className="info-label" infoLabel="Ingrese contraseña"/>
+        <input className='input-field' type='password' placeholder='Ingresar contraseña ' id='password'/>
+        
+        <button className='button'>Registrar</button>
+        
+      </form>
+
+      <h4 className='form-title'> Ya tienes cuenta?
+          <Link to='/Login' className='button'>
+              Inicia Sesion
+          </Link>
+      </h4>
+
       </div>
     );
     }
