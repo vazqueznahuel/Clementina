0a1bf9be96b3b1f412ea1cfee6fc75056df52084
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:31:02 -0300

    Añadiendo importaciones de route

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,3 +1,14 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import  Home from '../components/home/home'; 
+import Login from '../components/login/login';
+import Register from '../components/register/register';
+
+import appFirebase from '../firebase/firebaseConfig';
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+
+const auth = getAuth(appFirebase);
 
     function AppRouter() {
     return (
