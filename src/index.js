a7e82f75f677bfeaf1cb6efc1d651f0ee73ad5ed
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:31:59 -0300

    Añadiendo importaciones en index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
