8158e15969bb1be86011b458ad7f637ef4d97255
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:32:36 -0300

    Añadiendo importaciones en home.jsx

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,18 @@
-import '../../App.css'
+import { useState, useEffect } from 'react';
+import '../../App.css';
 import appFirebase from '../../firebase/firebaseConfig';
-import { getAuth, signOut } from 'firebase/auth';
+import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate desde react-router-dom
+import Nav from '../nav/nav';
+
 const auth = getAuth(appFirebase);
 
-function Home ({correoUsuario}){
+function Home({ correoUsuario }) {
     return (
-        <div className='container'>
-           <h1 className='text-center'>HOLA {correoUsuario}<button onClick={()=>signOut(auth)}>Logout</button></h1>
-        </div>
+        <>
+          
+        </>
     );
 }
+
 export default Home;
