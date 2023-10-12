dcce778ecb64218cf814ced9f5e07d8a2da4ff5e
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-10-12 19:20:48 -0300

    Añadiendo funciones y logica del componente login

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -15,6 +15,50 @@ const auth = getAuth(appFirebase);
 
 
 function Login(props) {
+  const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const functAutenticacion = async (e) =>{  
+      e.preventDefault();
+      const correo = e.target.email.value;
+      const contraseña = e.target.password.value;
+       
+        try {
+          await signInWithEmailAndPassword(auth, correo, contraseña);
+          navigate("/Home");
+        } catch (error) {
+          alert ("El correo es incorrecto");
+        }
+      
+  }
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
+  
+  const signInWithGithub = () => {
+    const provider = new GithubAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);  // Puedes acceder a la información del usuario aquí
+        navigate("/Home");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  };
 
     return (
     <>
