303ec1b6d838f17cf24d390e3ab5b1447e50d8bb
Facundo Hermosilla <facundohermosillaepet20@gmail.com>
2023-10-18 18:13:45 -0300

    Eliminando importaciones innecesarias

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
 import Label from '../label/label.jsx'
 import '../../App.css'
-
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-
-import Home from '../home/home.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle,faGithub } from '@fortawesome/free-brands-svg-icons';
 import { Link, useNavigate } from 'react-router-dom';
