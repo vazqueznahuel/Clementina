5144995d9a877cd281061c677f6162cbe67b09a5
Facundo Hermosilla <facundohermosillaepet20@gmail.com>
2023-10-18 18:13:03 -0300

    Agregando dependencias

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
