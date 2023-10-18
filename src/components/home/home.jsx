66e2904e872dab2c03583a6369d23721e6be761d
Facundo Hermosilla <facundohermosillaepet20@gmail.com>
2023-10-18 18:14:36 -0300

    Eliminando importaciones innecesarias y linea de codigo en home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,11 @@
-import { useState, useEffect } from 'react';
 import '../../App.css';
 import appFirebase from '../../firebase/firebaseConfig';
 import { getAuth, signOut, onAuthStateChanged } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom'; // Importa useNavigate desde react-router-dom
 import Nav from '../nav/nav';
 
 const auth = getAuth(appFirebase);
 
-function Home({ correoUsuario }) {
+function Home() {
     return (
         <>
           <Nav/>
