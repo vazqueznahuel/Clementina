3e82f3856b8eae9873d858311c304409da76855a
gabriel britos <you@example.com>
2023-10-25 15:51:35 -0300

    corrigiendo navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,13 +6,19 @@ function Navbar() {
     <nav className="navbar">
       <ul className="nav-list">
         <li className="nav-item">
-          <a href="/"><img src="/"/>Foto usuario</a>
+            {/* el link redirigiria al perfil de usuario, es decir, si toco la foto
+            de perfil me deberia llevar al perfil del usuario.
+            Esa imagen la puse para probar pero obvio iria una que elija el usuario */}
+          <a href="/"><img src={imagen1} height={90} width={90}/></a>
         </li>
         <li className="nav-item">
+            {/* nombre del usuario */}
           <a href="/">Nombre usuario</a>
         </li>
         <li className="nav-item">
-          <a href="/menú hamburguesa"><img src={imagen1}/></a>
+            {/* el link deberia desplejar el menú y la imagen en
+            realidad seria un menú hamburguesa */}
+          <a href="/menú hamburguesa"><img src={imagen1} height={50} width={50}/></a>
         </li>
       </ul>
     </nav>
