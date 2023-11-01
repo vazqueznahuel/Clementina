141050d999c1d1966059945ef4c1282d1bedd11f
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2023-10-31 22:04:19 -0300

    Se añadio el componente del qr

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import AppRouter from './routes/routes'
+import MyQRCodeComponent from './components/generateqr/generateqr'
 
 function App() {
 
     return ( 
         <div>
             <AppRouter/>
+            <MyQRCodeComponent/>
         </div>
     )
 }
