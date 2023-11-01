97fb8f7b03d99ed51e3e0edf95c89325ae3c9a4e
Luciana  Agüero <luciana.aguero.epet20@gmail.com>
2023-10-31 22:19:17 -0300

    Se agrego el qr de la epet6

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import AppRouter from './routes/routes'
 import Epet3qr from './components/generateqr/epet3qr'
 import Epet5qr from './components/generateqr/epet5qr'
+import Epet6qr from './components/generateqr/epet6qr'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
             <AppRouter/>
             <Epet3qr/>
             <Epet5qr/>
+            <Epet6qr/>
         </div>
     )
 }
