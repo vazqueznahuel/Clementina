b70bfb6a42ce7935cfd323b307006dc63d78455e
adrian vazquez <nahuel.vazquez@init-dev.com.ar>
2023-11-02 20:10:43 -0300

    Actualizando icon

--- src/components/Icon/icon.jsx
+++ src/components/Icon/icon.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
-import img1 from '../../Imagenes/imagen1.jpeg'
+import { Link } from 'react-router-dom';
+import img1 from '../../imagenes/imagen1.jpeg'
 import '../../css/sliderEpets.css'
 
-function Icon({nombre}) {
+function Icon({nombre, id}) {
     return (
         <div className='icon'>
             <div className="epets">
-                <a href="/ModelSlider" className='nav-epets'><img className='img' src={img1}/></a>
+            <Link to={`/ModelSlider/${id}`} className="nav-epets">
+          <img className="img" src={img1} alt="Icono" />
+        </Link>
                 <p className='nameEpets'>{nombre}</p>
             </div>
         </div>
