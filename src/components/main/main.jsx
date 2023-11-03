62df8e1b9e13db706d93456eec7f4fbf7494c9fd
HermosillaFacundo <facundohermosillaepet20@gmail.com>
2023-11-03 12:50:54 -0300

    Agregando boton temporal para actualizar datos delas

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Button from '../button/button';
 import Navbar from '../navbar/navbar';
 import Window from '../window/window';
@@ -6,7 +6,7 @@ import SliderEpets from '../sliderEpets/sliderEpets';
 import '../../css/main.css'
 import { getAuth, signOut } from 'firebase/auth';
 import appFirebase from '../../firebase/firebaseConfig.js'
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, updateDoc, getDoc, doc } from 'firebase/firestore';
 
 const auth = getAuth(appFirebase);
 const db = getFirestore(appFirebase);
@@ -16,12 +16,45 @@ function Main() {
         alert('El botón fue clickeado');
     };
 
+    const [widi, setWidi] = useState(false);
+    const [user, setUser] = useState(null);
+  
+    useEffect(() => {
+      const unsubscribe = auth.onAuthStateChanged(async (user) => {
+        setUser(user);
+        if (user) {
+          const docRef = doc(db, "users", user.uid);
+          const docSnap = await getDoc(docRef);
+          if (docSnap.exists()) {
+            setWidi(docSnap.data().Widi);
+          } else {
+            console.log("No such document!");
+          }
+        }
+      });
+  
+      return () => unsubscribe();
+    }, []);
+  
+    const toggleWidi = async () => {
+      const newValue = !widi;
+      setWidi(newValue);
+  
+      const docRef = doc(db, "users", user.uid);
+      await updateDoc(docRef, {
+        Widi: newValue
+      });
+    }
+
     return (
         <div className='main'>
             <Navbar/>
             <Window/>
             <Button text = "Escanear QR" onClick = { handleClick }/>
             <SliderEpets/>
+            <button onClick={toggleWidi}>
+                {widi ? 'Desactivar Widi' : 'Activar Widi'}
+            </button>
         </div>
     );
 }
