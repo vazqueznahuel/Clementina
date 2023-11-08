0c3dcea108c147b942d9c15bb45fde2bd6e1ecbd
adrian vazquez <nahuel.vazquez@init-dev.com.ar>
2023-11-07 22:34:09 -0300

    corrigiendo errores

--- src/components/Icon/icon.jsx
+++ src/components/Icon/icon.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
-import img1 from '../../imagenes/coala.png'
 import '../../css/sliderEpets.css'
 
 function Icon({nombre, id}) {
