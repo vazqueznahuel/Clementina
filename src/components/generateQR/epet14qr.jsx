38da9c04cd88b7deee03d0412139d356b21be313
Rosario <rosario.gonzalez.epet20@gmail.com>
2023-11-16 17:39:26 -0300

    QR epet14

--- src/components/generateQR/epet14qr.jsx
+++ src/components/generateQR/epet14qr.jsx
@@ -5,7 +5,15 @@ function Epet14qr() {
     const qrData = ''
     return (
         <div>
-            <QRCode value={qrData}/>
+            {/* Segunda instancia con opciones adicionales */}
+            <QRCode
+                value="Anguila"
+                size={128}  // Tamaño en píxeles
+                bgColor="#FFFFFF"  // Color de fondo
+                fgColor="#000000"  // Color de primer plano
+                level="L"  // Nivel de corrección de errores (L, M, Q, H)
+                className='qr'
+            />
         </div>
     );
 
