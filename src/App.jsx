36f2ee8a7bde04d42ee051fa25fa3a9b1fd4bf5e
Rosario <rosario.gonzalez.epet20@gmail.com>
2023-11-16 17:44:31 -0300

    Agregando escaner de cada epet

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import AppRouter from './routes/routes'
 import Epet20qr from './components/generateQR/epet20qr';
+import Epet3qr from './components/generateQR/epet3qr';
+import Epet8qr from './components/generateQR/epet8qr';
+import Epet5qr from './components/generateQR/epet5qr';
+import Epet6qr from './components/generateQR/epet6qr';
+import Epet17qr from './components/generateQR/epet17qr';
+import Epet14qr from './components/generateQR/epet14qr';
+
 
 
 function App() {
@@ -9,6 +16,12 @@ function App() {
         <div>
             <AppRouter/>
             <Epet20qr/>
+            <Epet3qr/>
+            <Epet5qr/>
+            <Epet6qr/>
+            <Epet8qr/>
+            <Epet17qr/>
+            <Epet14qr/>
         </div>
     )
 }
