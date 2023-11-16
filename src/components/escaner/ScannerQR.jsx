c2a17bc20c2fa57a4be900336fb447df1a4aef9b
gabriel britos <you@example.com>
2023-11-16 10:30:16 -0300

    arreglos

--- src/components/escaner/ScannerQR.jsx
+++ src/components/escaner/ScannerQR.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { QrReader } from 'react-qr-reader';
 
-const QRScanner = ({ onScan, onClose }) => {
+const QRScanner = ({ onClose }) => {
   const [cameraActive, setCameraActive] = useState(true);
   const [facingMode, setFacingMode] = useState('environment');
   const qrReaderRef = useRef(null);
 
-  const handleScan = (data) => {
-    if (data) {
-      onScan(data);
+  const handleResult = (result) => {
+    if (result) {
+      console.log('Resultado del escaneo:', result);
+      // Realizar acciones adicionales si es necesario
       setCameraActive(false);
     }
   };
@@ -37,7 +38,7 @@ const QRScanner = ({ onScan, onClose }) => {
             ref={qrReaderRef}
             delay={100}
             onError={handleError}
-            onScan={handleScan}
+            onResult={handleResult}  // Cambiado a onResult
             facingMode={facingMode}
             style={{ width: '100%' }}
           />
