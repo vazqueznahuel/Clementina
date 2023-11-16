8249808a8f8df0e4819035bd0b0982dd95449a33
Rosario <rosario.gonzalez.epet20@gmail.com>
2023-11-16 17:38:20 -0300

    QR epet5

--- src/components/generateQR/epet5qr.jsx
+++ src/components/generateQR/epet5qr.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 
 function Epet5qr() {
-    const qrData = ''
+    
 
     return (
         <div>
-         <QRCode value={qrData}/>
+            {/* Segunda instancia con opciones adicionales */}
+            <QRCode
+                value="Calamaria"
+                size={128}  // Tamaño en píxeles
+                bgColor="#FFFFFF"  // Color de fondo
+                fgColor="#000000"  // Color de primer plano
+                level="L"  // Nivel de corrección de errores (L, M, Q, H)
+                className='qr'
+            />
         </div>
     );
 }
