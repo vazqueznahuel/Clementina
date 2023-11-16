806a5620bd09099b4e631b2cb6908533784b35b9
Rosario <rosario.gonzalez.epet20@gmail.com>
2023-11-16 02:38:32 -0300

    Probando el redireccionamiento de qr

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AppRouter from './routes/routes'
+import Epet20qr from './components/generateQR/epet20qr';
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     return ( 
         <div>
             <AppRouter/>
+            <Epet20qr/>
         </div>
     )
 }
