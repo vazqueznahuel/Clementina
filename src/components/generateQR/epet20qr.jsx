4b56f3a15c156a3dcfa70a3eeb317d7ce2006ba8
Rosario <rosario.gonzalez.epet20@gmail.com>
2023-11-16 17:38:56 -0300

    QR epet20

--- src/components/generateQR/epet20qr.jsx
+++ src/components/generateQR/epet20qr.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 
 function Epet20qr() {
-    const qrData = 'https://e-pets-clementina.netlify.app/ModelSlider/0';
 
     return (
         <div>
             {/* Segunda instancia con opciones adicionales */}
             <QRCode
-                value={qrData}
+                value="Widi"
                 size={128}  // Tamaño en píxeles
                 bgColor="#FFFFFF"  // Color de fondo
                 fgColor="#000000"  // Color de primer plano
                 level="L"  // Nivel de corrección de errores (L, M, Q, H)
+                className='qr'
             />
         </div>
     );
