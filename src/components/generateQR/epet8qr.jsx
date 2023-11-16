fbbc92c0a63b768a794118ae4a89860247afb566
Rosario <rosario.gonzalez.epet20@gmail.com>
2023-11-16 17:37:53 -0300

    QR epet8

--- src/components/generateQR/epet8qr.jsx
+++ src/components/generateQR/epet8qr.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 
 function Epet8qr() {
-    const qrData = ''
+
     return (
         <div>
-            <QRCode value={qrData}/>
+            {/* Segunda instancia con opciones adicionales */}
+            <QRCode
+                value="Dozer"
+                size={128}  // Tamaño en píxeles
+                bgColor="#FFFFFF"  // Color de fondo
+                fgColor="#000000"  // Color de primer plano
+                level="L"  // Nivel de corrección de errores (L, M, Q, H)
+                className='qr'
+            />
         </div>
     );
 }
